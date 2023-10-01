f06d8f20aaf1c3f1decd94a23f89becb45d854e0
JohnGudron <oak43556@gmail.com>
2023-10-02 00:09:07 +0300

    Code for second stage but without checking tests.

--- Aquarium/task/src/main/java/org/hyperskill/aquarium/MainActivity.kt
+++ Aquarium/task/src/main/java/org/hyperskill/aquarium/MainActivity.kt
@@ -2,19 +2,31 @@ package org.hyperskill.aquarium
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 import android.widget.ImageView
 import android.widget.TextView
+import com.squareup.picasso.Picasso
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        var currentPage = 0
+
         val imageAnimals =
-            (intent.getSerializableExtra("imageAnimals") ?: listOf(R.drawable.image1)) as List<Int>
+            (intent.getSerializableExtra("imageAnimals") ?: listOf(
+                "https://ucarecdn.com/42045846-b968-4a88-81ec-df73bec4fcb7/",
+
+                "https://ucarecdn.com/5aa10eb3-fc49-4304-9057-adf1d29a9b4c/",
+
+                "https://ucarecdn.com/c5fd39b9-7690-4616-b7dc-d3f8da883146/"
+            )) as List<String>
         val nameAnimals =
-            (intent.getSerializableExtra("nameAnimals") ?: listOf("Koi Carp")) as List<String>
+            (intent.getSerializableExtra("nameAnimals") ?: listOf("Koi Carp", "Spiny Dogfish", "Kaluga")) as List<String>
         val descriptionAnimals = (intent.getSerializableExtra("descriptionAnimals") ?: listOf(
+
+            //Koi Carp
             "These colorful, ornamental fish are a variety of the Amur carp. " +
                     "They were originally found in Central Europe and Asia, " +
                     "but they’ve spread to many other parts of the world. " +
@@ -25,10 +37,66 @@ class MainActivity : AppCompatActivity() {
                     "Carps bred outside of Japan have an average lifespan of around 15 years," +
                     " while carps bred in Japan can live 40 years or more. The oldest koi carp on record," +
                     " which was a fish named Hanako, reportedly lived for 226 years!",
+
+            // Spiny dogfish
+            "The spiny dogfish is a type of shark with venomous spines in front of its dorsal fins." +
+                    " Not only is it an aggressive hunter, but these fish are known to hunt in packs!" +
+                    " Like many shark species, these fish grow slowly, " +
+                    "and some females don’t reach full maturity until they’re over 30 years old.\n" +
+                    "\n" +
+                    "While the lifespan of the spiny dogfish is already impressive, " +
+                    "some fish live for far longer than average. " +
+                    "Spiny dogfish in the Pacific Ocean tend to live longer than fish in the Atlantic," +
+                    " with some fish living longer than 80 years. " +
+                    "Females tend to mature later than males, and they usually live longer too.",
+
+            //Kaluga
+            "Sometimes called the river beluga, the kaluga is a type of predatory sturgeon." +
+                    " While these fish spend the majority of their time in freshwater, " +
+                    "they’re also able to survive in salt water. " +
+                    "The kaluga is one of the world’s largest freshwater fish species and can grow to be more than 18 feet long, " +
+                    "with a weight of over 2,200 pounds.\n" +
+                    "\n" +
+                    "Kalugas are overfished, which has left the species vulnerable to extinction. " +
+                    "Although many kaluga are killed before they fully mature, " +
+                    "these fish have the potential to live very long lives. " +
+                    "One kaluga that was caught in China is estimated to be over 100 years old."
         )) as List<String>
 
+        val nameView = findViewById<TextView>(R.id.tv_name)
+        val descriptionView = findViewById<TextView>(R.id.tv_description)
+        val imageView = findViewById<ImageView>(R.id.image_view)
+        val nextBtn = findViewById<Button>(R.id.btn_next)
+        val prevBtn = findViewById<Button>(R.id.btn_previous)
 
-        findViewById<TextView>(R.id.tv_name).text = nameAnimals[0]
-        findViewById<TextView>(R.id.tv_description).text = descriptionAnimals[0]
+        nameView.text = nameAnimals[0]
+        descriptionView.text = descriptionAnimals[0]
+
+        nextBtn.setOnClickListener {
+            currentPage = if ((currentPage + 1) > nameAnimals.lastIndex) 0 else currentPage + 1
+            Picasso.get()
+                .load(imageAnimals[currentPage])
+                .fit().centerCrop()
+                .placeholder(R.drawable.placeholder)
+                .error(R.drawable.error)
+                .into(imageView)
+
+            nameView.text = nameAnimals[currentPage]
+            descriptionView.text = descriptionAnimals[currentPage]
+
+        }
+
+        prevBtn.setOnClickListener {
+            currentPage = if ((currentPage - 1) < 0) nameAnimals.lastIndex else currentPage - 1
+            Picasso.get()
+                .load(imageAnimals[currentPage])
+                .fit().centerCrop()
+                .placeholder(R.drawable.placeholder)
+                .error(R.drawable.error)
+                .into(imageView)
+
+            nameView.text = nameAnimals[currentPage]
+            descriptionView.text = descriptionAnimals[currentPage]
+        }
     }
 }
